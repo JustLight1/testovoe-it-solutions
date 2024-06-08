d0024c3a4c42e04b4880b7d7d69539b87339b17b
Форов Александр <5h9t5@list.ru>
2024-06-08 19:23:06 +0300

    feat(posts): add model admin for model Post

--- src/posts/admin.py
+++ src/posts/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'author', 'views_count', 'position')
+    list_filter = ('position',)
+    search_fields = ('title',)
+    ordering = ('position',)
