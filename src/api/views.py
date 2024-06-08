c52f73d74b1daea53c5eaeec5112b0510507c9ff
Форов Александр <5h9t5@list.ru>
2024-06-08 19:25:50 +0300

    feat(api): add view PostsList and PostDetail

--- src/api/views.py
+++ src/api/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from posts.models import Post
+from src import settings
+
+from .serializers import PostSerializer
+
+
+class PostsList(generics.ListAPIView):
+    queryset = Post.objects.all().order_by(
+        'position')[:settings.MAX_POSTS_COUNT]
+    serializer_class = PostSerializer
+
+
+class PostDetail(generics.RetrieveAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    lookup_field = 'id'
