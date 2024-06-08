45ae5b24a3344c8a55b0b7542d28b97d6f08c0c9
Форов Александр <5h9t5@list.ru>
2024-06-08 19:22:15 +0300

    feat(posts): add model Post

--- src/posts/models.py
+++ src/posts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField('Название', max_length=150)
+    author = models.CharField('Автор', max_length=50)
+    views_count = models.PositiveIntegerField('Количество просмотров',
+                                              default=0)
+    position = models.PositiveIntegerField('Позиция объявления', default=None)
+
+    class Meta:
+        ordering = ['position']
+        verbose_name = 'Объявление'
+        verbose_name_plural = 'Объявления'
+
+    def __str__(self):
+        return self.title
